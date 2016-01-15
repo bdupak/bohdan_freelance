7a18bbcc8a3dc35b4fde0eccf91c4142f4862d51
Ip1x <mrudevich96@gmail.com>
2016-01-16 00:14:52 +0200

    models part 2

--- business/src/main/java/com/epam/freelancer/model/Admin.java
+++ business/src/main/java/com/epam/freelancer/model/Admin.java
@@ -1,10 +1,10 @@
 package com.epam.freelancer.model;
 
 import java.util.Locale;
-
 /**
  * Created by Максим on 15.01.2016.
  */
+
 public class Admin implements UserEntity {
 
     private Integer id;
@@ -14,12 +14,22 @@ public class Admin implements UserEntity {
     private String lname;
 
     private String lang;
+    private Locale locale;
     private String uuid;
 
-    public String getLang() {
-        return lang;
+    public Locale getLocale() {
+        return locale;
     }
 
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public String getLang() {
+        return lang != null ? locale.toLanguageTag() : null;
+    }
+
+    @Override
     public void setLang(String lang) {
         this.lang = lang;
     }
@@ -52,16 +62,6 @@ public class Admin implements UserEntity {
     @Override
     public void setUuid(String uuid) {
         this.uuid = uuid;
-    }
-
-    @Override
-    public Locale getLocale() {
-        return null;
-    }
-
-    @Override
-    public void setLocale(Locale locale) {
-
     }
 
     @Override
