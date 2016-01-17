4d74e1d99583efdc96f716fb0f39bbafe921ddea
TenebrisFx <bohdandarkzolochiv@gmail.com>
2016-01-17 20:44:57 +0200

    Validator now support numbers

--- business/src/main/java/com/epam/freelancer/business/util/ValidationParametersBuilder.java
+++ business/src/main/java/com/epam/freelancer/business/util/ValidationParametersBuilder.java
@@ -2,17 +2,24 @@ package com.epam.freelancer.business.util;
 
 public abstract class ValidationParametersBuilder {
 
-	public static Parameters createParameters() {
-		return new Parameters();
+	public static Parameters createParameters(boolean isNumber) {
+		return new Parameters(isNumber);
 	}
 
 	public static class Parameters {
+		private boolean isNumber = false;
+
+		private Boolean isInteger;
+		private Double max;
+		private Double min;
+
 		private Integer maxLength;
 		private Integer minLength;
 		private String pattern;
 		private Boolean notEmptyString;
 
-		private Parameters() {
+		private Parameters(boolean isNumber) {
+			this.isNumber = isNumber;
 		}
 
 		public ValidationParametersBuilder.Parameters maxLength(int maxLength) {
@@ -51,6 +58,37 @@ public abstract class ValidationParametersBuilder {
 
 		public Boolean getNotEmptyString() {
 			return notEmptyString;
+		}
+
+		public boolean isNumber() {
+			return isNumber;
+		}
+
+		public Boolean getIsInteger() {
+			return isInteger;
+		}
+
+		public Double getMax() {
+			return max;
+		}
+
+		public Double getMin() {
+			return min;
+		}
+
+		public ValidationParametersBuilder.Parameters isInteger(Boolean isInteger) {
+			this.isInteger = isInteger;
+			return this;
+		}
+
+		public ValidationParametersBuilder.Parameters max(Double max) {
+			this.max = max;
+			return this;
+		}
+
+		public ValidationParametersBuilder.Parameters min(Double min) {
+			this.min = min;
+			return this;
 		}
 	}
 }
--- business/src/main/java/com/epam/freelancer/business/util/Validator.java
+++ business/src/main/java/com/epam/freelancer/business/util/Validator.java
@@ -6,6 +6,78 @@ public abstract class Validator {
 	public static boolean validate(Parameters validationParam,
 			String... strings)
 	{
+		if (validationParam.isNumber())
+			return validateNumbers(validationParam, strings);
+		else
+			return validateStrings(validationParam, strings);
+	}
+
+	private static boolean validateNumbers(Parameters validationParam,
+			String[] strings)
+	{
+		if (strings == null || strings.length == 0)
+			return false;
+		
+		if (validationParam.getIsInteger() == null
+				|| validationParam.getIsInteger())
+			try {
+				return validateInteger(validationParam, strings);
+			} catch (Exception e) {
+				return false;
+			}
+		else
+			try {
+				return validateDouble(validationParam, strings);
+			} catch (Exception e) {
+				return false;
+			}
+	}
+
+	private static boolean validateDouble(Parameters validationParam,
+			String[] strings)
+	{
+		Double[] numbers = new Double[strings.length];
+		for (int i = 0; i < strings.length; i++)
+			numbers[i] = Double.valueOf(strings[i]);
+
+		if (validationParam.getMax() != null)
+			for (Double integer : numbers)
+				if (integer == null
+						|| Double.compare(integer, validationParam.getMax()) >= 0)
+					return false;
+		if (validationParam.getMin() != null)
+			for (Double integer : numbers)
+				if (integer == null
+						|| Double.compare(integer, validationParam.getMin()) < 0)
+					return false;
+		return true;
+	}
+
+	private static boolean validateInteger(Parameters validationParam,
+			String[] strings)
+	{
+		Integer[] numbers = new Integer[strings.length];
+		for (int i = 0; i < strings.length; i++)
+			numbers[i] = Integer.valueOf(strings[i]);
+
+		if (validationParam.getMax() != null)
+			for (Integer integer : numbers)
+				if (integer == null
+						|| Integer.compare(integer, validationParam.getMax()
+								.intValue()) >= 0)
+					return false;
+		if (validationParam.getMin() != null)
+			for (Integer integer : numbers)
+				if (integer == null
+						|| Integer.compare(integer, validationParam.getMin()
+								.intValue()) < 0)
+					return false;
+		return true;
+	}
+
+	private static boolean validateStrings(Parameters validationParam,
+			String[] strings)
+	{
 		if (validationParam.getNotEmptyString() != null)
 			if (validationParam.getNotEmptyString()) {
 				for (String string : strings)
@@ -37,4 +109,5 @@ public abstract class Validator {
 
 		return true;
 	}
+
 }
