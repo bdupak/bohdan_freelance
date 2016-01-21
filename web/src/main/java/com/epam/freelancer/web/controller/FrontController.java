59771f582053807f66f2f2eacf6cf42096930ed7
volodimyr <l.volodimyr@gmail.com>
2016-01-21 15:40:02 +0200

    Add authorization action

--- web/src/main/java/com/epam/freelancer/web/controller/FrontController.java
+++ web/src/main/java/com/epam/freelancer/web/controller/FrontController.java
@@ -1,5 +1,18 @@
 package com.epam.freelancer.web.controller;
 
+import com.epam.freelancer.business.context.ApplicationContext;
+import com.epam.freelancer.business.service.UserService;
+import com.epam.freelancer.database.model.UserEntity;
+import com.epam.freelancer.security.provider.AuthenticationProvider;
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -8,20 +21,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.ResourceBundle;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.log4j.Logger;
-import org.codehaus.jackson.map.ObjectMapper;
-
 public class FrontController extends HttpServlet {
-	private final static Logger LOG = Logger.getLogger(FrontController.class);
-	private static final long serialVersionUID = 1L;
-	private Map<String, HttpServlet> controllers = new HashMap<>();
+    private final static Logger LOG = Logger.getLogger(FrontController.class);
+    private static final long serialVersionUID = 1L;
+    private Map<String, HttpServlet> controllers = new HashMap<>();
 
     public static String getPath(HttpServletRequest request) {
         return request.getRequestURI()
@@ -29,87 +32,90 @@ public class FrontController extends HttpServlet {
                 .substring("/front/".length());
     }
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		LOG.info(getClass().getSimpleName() + " - " + "front controller loaded");
-		super.init(config);
-		configControllers();
-	}
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        LOG.info(getClass().getSimpleName() + " - " + "front controller loaded");
+        ApplicationContext.getInstance().addBean("authenticationProvider", new AuthenticationProvider());
+        super.init(config);
+        configControllers();
+    }
 
     private void configControllers() {
         controllers.put("user/", new UserController());
     }
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException
-	{
-		LOG.info(getClass().getSimpleName() + " - " + "doGet");
-		try {
-			if (request.getSession().isNew())
-				configAutoAuthentication(request.getSession());
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        LOG.info(getClass().getSimpleName() + " - " + "doGet");
+        try {
+            if (request.getSession().isNew())
+                configAutoAuthentication(request.getSession());
 
-			String path = request.getRequestURI().substring(
-					request.getContextPath().length());
+            String path = request.getRequestURI().substring(
+                    request.getContextPath().length());
 
-			if (path.startsWith("/front/")) {
-				path = path.substring("/front/".length());
-				if ("".equals(path))
-					path = "home";
+            if (path.startsWith("/front/")) {
+                path = path.substring("/front/".length());
+                if ("".equals(path))
+                    path = "home";
 
-				if (path.startsWith("admin/")) {
-					controllers.get("admin/").service(request, response);
-					return;
-				}
-				if (path.startsWith("dev/")) {
-					controllers.get("dev/").service(request, response);
-					return;
-				}
-				if (path.startsWith("cust/")) {
-					controllers.get("cust/").service(request, response);
-					return;
-				}
-				if (path.equals("language/bundle")) {
-					sendBundle(request, response);
-					return;
-				}
-				request.getRequestDispatcher("/views/" + path + ".jsp")
-						.forward(request, response);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			LOG.fatal(getClass().getSimpleName() + " - " + "doGet");
-		}
-	}
+                if (path.startsWith("admin/")) {
+                    controllers.get("admin/").service(request, response);
+                    return;
+                }
+                if (path.startsWith("dev/")) {
+                    controllers.get("dev/").service(request, response);
+                    return;
+                }
+                if (path.startsWith("cust/")) {
+                    controllers.get("cust/").service(request, response);
+                    return;
+                }
+                if (path.equals("language/bundle")) {
+                    sendBundle(request, response);
+                    return;
+                }
+                if (path.equals("logout")) {
+                    logout(request, response);
+                    return;
+                }
+                request.getRequestDispatcher("/views/" + path + ".jsp")
+                        .forward(request, response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOG.fatal(getClass().getSimpleName() + " - " + "doGet");
+        }
+    }
 
-	private void sendBundle(HttpServletRequest request,
-			HttpServletResponse response)
-	{
-		LOG.info(getClass().getSimpleName() + " - " + "sendBundle");
-		Locale locale = ((Locale) request.getSession().getAttribute("language"));
-		ResourceBundle bundle = null;
-		if (locale != null)
-			bundle = ResourceBundle.getBundle("/i18n/language", locale);
-		else
-			bundle = ResourceBundle.getBundle("/i18n/language", Locale.ENGLISH);
+    private void sendBundle(HttpServletRequest request,
+                            HttpServletResponse response) {
+        LOG.info(getClass().getSimpleName() + " - " + "sendBundle");
+        Locale locale = ((Locale) request.getSession().getAttribute("language"));
+        ResourceBundle bundle = null;
+        if (locale != null)
+            bundle = ResourceBundle.getBundle("/i18n/language", locale);
+        else
+            bundle = ResourceBundle.getBundle("/i18n/language", Locale.ENGLISH);
 
-		Map<String, String> bundleMap = new HashMap<>();
+        Map<String, String> bundleMap = new HashMap<>();
 
-		for (String key : bundle.keySet()) {
-			String value = bundle.getString(key);
-			bundleMap.put(key, value);
-		}
+        for (String key : bundle.keySet()) {
+            String value = bundle.getString(key);
+            bundleMap.put(key, value);
+        }
 
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		try (PrintWriter out = response.getWriter()) {
-			out.print(new ObjectMapper().writeValueAsString(bundleMap));
-			out.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.print(new ObjectMapper().writeValueAsString(bundleMap));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void configAutoAuthentication(HttpSession session) {
+    private void configAutoAuthentication(HttpSession session) {
         /*LOG.info(getClass().getSimpleName() + " - "
                 + "configAutoAuthentication");
 		EnvironmentVariablesManager manager = EnvironmentVariablesManager
@@ -119,17 +125,16 @@ public class FrontController extends HttpServlet {
 		session.setAttribute(manager.getVar("session.cust.autoauth"), 1);*/
     }
 
-	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException
-	{
-		LOG.info(getClass().getSimpleName() + " - " + "doPost");
-		try {
-			String path = request.getRequestURI().substring(
-					request.getContextPath().length());
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        LOG.info(getClass().getSimpleName() + " - " + "doPost");
+        try {
+            String path = request.getRequestURI().substring(
+                    request.getContextPath().length());
 
-			if (path.startsWith("/front/")) {
-				path = path.substring("/front/".length());
+            if (path.startsWith("/front/")) {
+                path = path.substring("/front/".length());
                 /*if (path.startsWith("admin/")) {
                     controllers.get("admin/").service(request, response);
 					return;
@@ -147,19 +152,33 @@ public class FrontController extends HttpServlet {
                     return;
                 }
                 controllers.get(path).service(request, response);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			LOG.fatal(getClass().getSimpleName() + " - " + "doPost");
-		}
-	}
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOG.fatal(getClass().getSimpleName() + " - " + "doPost");
+        }
+    }
 
-	@Override
-	public void destroy() {
-		LOG.info(getClass().getSimpleName() + " - " + "destroy");
-		super.destroy();
-		for (Entry<String, HttpServlet> controller : controllers.entrySet()) {
-			controller.getValue().destroy();
-		}
-	}
+    @Override
+    public void destroy() {
+        LOG.info(getClass().getSimpleName() + " - " + "destroy");
+        super.destroy();
+        for (Entry<String, HttpServlet> controller : controllers.entrySet()) {
+            controller.getValue().destroy();
+        }
+    }
+
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        LOG.info(getClass().getSimpleName() + " - " + "logout");
+        UserEntity userEntity = (UserEntity) request.getSession()
+                .getAttribute("user");
+        AuthenticationProvider authenticationProvider = (AuthenticationProvider) ApplicationContext
+                .getInstance().getBean("authenticationProvider");
+        authenticationProvider.invalidateUserCookie(response,
+                "freelancerRememberMeCookie", userEntity);
+        UserService userService = (UserService) ApplicationContext.getInstance().getBean("userService");
+        userService.modify(userEntity);
+        request.getSession().removeAttribute("user");
+        response.sendRedirect(request.getContextPath() + "/");
+    }
 }
