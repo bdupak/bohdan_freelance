749f5758696aaf24ca2dc08c9fa81113e6a07dea
Ip1x <mrudevich96@gmail.com>
2016-01-21 19:24:27 +0200

    registration finish

--- web/src/main/java/com/epam/freelancer/web/controller/UserController.java
+++ web/src/main/java/com/epam/freelancer/web/controller/UserController.java
@@ -29,7 +29,6 @@ public class UserController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         try {
             switch (FrontController.getPath(request)) {
                 case "user/email":
