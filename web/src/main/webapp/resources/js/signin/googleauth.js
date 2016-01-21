c8323e3dde252ebd7607ab23ca5b8470d0bca06c
Volodimyr Lominsky <l.volodimyr@gmail.com>
2016-01-21 22:29:18 +0200

    Delete googleauth.js

--- web/src/main/webapp/resources/js/signin/googleauth.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $('#logout').click(function () {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-            console.log('User signed out.');
-        });
-    });
-});
